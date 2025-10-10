5533fd97c2e65df37a9570d7bd95d4dfc9cecf44
m-dkhan <mdzaidkhan628@gmail.com>
2025-10-10 15:05:05 -0400

    Added Todo Service

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 //src/app.js
 
 const express = require('express')
-const todoRoutes = require('./api/v1/routes/todoRoutes');
+const dbConfig = require('./config/database');
+const createTodoRepository = require('./api/v1/repositories/todoRepository');
+const {createTodoService} = require('./api/v1/services/todoService');
+const createTodoRouter = require('./api/v1/routes/todoRoutes');
+const knex = require('knex');
 
 // create an instance of express application
 const app = express();
@@ -9,7 +13,16 @@ const app = express();
 // enable express to receive json
 app.use(express.json());
 
-app.use('/api/v1/todos', todoRoutes);
+// create the db connection 
+const db = knex(dbConfig.development)
+
+// create the repository instance
+const todoRepository = createTodoRepository(db);
+
+// create the todo service instance 
+const todoService = createTodoService(todoRepository);
+
+app.use('/api/v1/todos', createTodoRouter(todoService));
 
 // export the app so tha`t tests can use it 
 module.exports = app;
